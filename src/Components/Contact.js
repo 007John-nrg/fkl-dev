cae0ccd7dd1f0c69fd5d5c9de090f9f4f528695e
Elvis <45665873+elviskio@users.noreply.github.com>
2024-01-25 11:41:19 +0300

    Make changes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import './components.css'
+import OpenHours from './OpenHours'
 
 const Contact = () => {
   return (
-    <div>Contact us</div>
+    <div className="contactBox">
+      <div className="sectionTitle">
+        <h5>CONTACT US</h5>
+      </div>
+      <OpenHours />
+    </div>
   )
 }
 
