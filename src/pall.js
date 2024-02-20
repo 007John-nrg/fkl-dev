6e03c5ef61e4bfc94dc8d7ffce1955e448f501b8
Elvis <45665873+elviskio@users.noreply.github.com>
2024-02-20 12:13:40 +0300

    changes

--- src/pall.js
+++ src/pall.js
@@ -49,7 +49,6 @@ import { Link } from 'react-router-dom';
 const Pall = () => {
   const [popupImages, setPopupImages] = useState([]);
   const [showPopup, setShowPopup] = useState(false);
-  const [pdfFilePath, setPdfFilePath] = useState('');
 
   const P11 = [p, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12];
   const P22 = [p13, p14, p15, p16, p160, p17, p18];
@@ -100,7 +99,7 @@ const Pall = () => {
           pdf.addPage(); // Add a new page for the next image
         }
       }
-      pdf.save('download.pdf'); // Save the PDF with a specified name
+      pdf.save('palladium.pdf'); // Save the PDF with a specified name
     } catch (error) {
       console.error('Error converting images to PDF:', error);
     }
@@ -163,7 +162,7 @@ const Pall = () => {
 
   return (
     <div>
-        <div>
+        <div style={{}}>
             {[
                 { image: P11[0], category: 'P11' },
                 { image: P22[0], category: 'P22' },
@@ -174,8 +173,8 @@ const Pall = () => {
                 { image: P77[0], category: 'P77' },
                 { image: P88[0], category: 'P88' },
             ].map((item, index) => (
-                <div key={index}>
-                    <div className="imgpally img-body" style={{marginTop: '3rem', width: '150px'}}>
+                <div key={index} style={{marginTop: '1rem'}}>
+                    <div className="imgpally img-body" style={{width: '250px'}}>
                         <img src={item.image} alt="portfolio image" />
                         <div className="img-overlay">
                             <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" onClick={() => handleEyeIconClick(item.category)}><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg>
@@ -186,7 +185,7 @@ const Pall = () => {
             ))}
         </div>
         {showPopup && (
-        <div className="popup-overlay">
+        <div className="popup-overlay" style={{zIndex: '999999999'}}>
             <div className="popup">
                 <button onClick={() => setShowPopup(false)}>X</button>
                 <div className="flipbook-container">
